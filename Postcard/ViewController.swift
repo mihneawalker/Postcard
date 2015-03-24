d008f56cbfaefe43efa83e1349e374dc8509e43b
mihnea <mihnea@Annas-MacBook-Air.local>
2015-03-24 02:36:13 +0200

    added a comment

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -8,18 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var enterNameTextField: UITextField!
+    @IBOutlet weak var enterMessageTextField: UITextField!
+    
+    @IBOutlet weak var sendMailButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        enterMessageTextField.delegate = self
+        enterNameTextField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        //testing git shit
     }
 
 
+    @IBAction func sendMailButtonPressed(sender: UIButton) {
+        messageLabel.hidden = false
+        messageLabel.text = enterNameTextField.text + enterMessageTextField.text
+        messageLabel.textColor = UIColor.blueColor()
+        
+        
+        enterMessageTextField.resignFirstResponder()
+        sendMailButton.setTitle("mail sent", forState: UIControlState.Normal)
+    }
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        if (textField == enterNameTextField) {
+            enterMessageTextField.becomeFirstResponder()
+            return false
+        }
+        
+        if (textField == enterMessageTextField) {
+            sendMailButtonPressed(sendMailButton)
+        }
+        return true
+    }
 }
 
